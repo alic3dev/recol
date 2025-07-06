40a32e236663ae372933d4712e62b0e70c0f15d7
alic3 <alic3dev@gmail.com>
2025-07-06 00:50:18 -0700

    Initial Commit

---
+++ recol/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  recol
+//
+//  Created by Alice Grace on 7/6/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ recol/GameViewController.h
@@ -0,0 +1,16 @@
+//
+//  GameViewController.h
+//  recol
+//
+//  Created by Alice Grace on 7/6/25.
+//
+
+#import <UIKit/UIKit.h>
+#import <Metal/Metal.h>
+#import <MetalKit/MetalKit.h>
+#import "Renderer.h"
+
+// Our iOS view controller
+@interface GameViewController : UIViewController
+
+@end
